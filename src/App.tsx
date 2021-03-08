1b2d04af3c437eb7722afa5cb671b9fdf34ec80b
MohammadSafia <msafia@terkwaz.com>
2021-03-08 04:31:32 +0200

    Add style-components, Add google fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {createGlobalStyle} from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`
 
 const App: React.FC = () => {
     return (
-        <div className="App">
-
-        </div>
+        <>
+            <h1>Hello</h1>
+            <GlobalStyle/>
+            <div>Hello world</div>
+        </>
     );
 }
 
