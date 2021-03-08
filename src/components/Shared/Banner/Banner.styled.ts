a248d50211dc6726eb97f66101f183b43e8c438c
MohammadSafia <msafia@terkwaz.com>
2021-03-08 05:07:25 +0200

    Add Filter for banner

--- src/components/Shared/Banner/Banner.styled.ts
+++ src/components/Shared/Banner/Banner.styled.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
 export const BannerStyled = styled.div`
-  height: 200px;
+  height: 32rem;
   border: 1px solid red;
   background-image: url("images/banner.jpg");
   background-size: cover;
   background-position: center;
+  filter: contrast(75%);
 `
--- src/components/Shared/Navbar/Navbar.styled.ts
+++ src/components/Shared/Navbar/Navbar.styled.ts
@@ -8,6 +8,7 @@ export const NavbarStyled = styled.div`
   padding: 10px;
   position: fixed;
   width: 100%;
+  z-index: 999;
 `
 
 export const Logo = styled(Title)`
